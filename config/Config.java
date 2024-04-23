3ffffffd449e941e5fdb48fe2929c7e9f3ba1d31
benjmnxu <benxu@seas.upenn.edu>
2024-04-23 19:55:18 +0000

    RDS and some tables (chat, chatMessage, comments, likes)

--- config/Config.java
+++ config/Config.java
@@ -2,24 +2,25 @@ package config;
 
 public class Config {
 
-    public static final String DATABASE_CONNECTION = "jdbc:mysql://localhost:3306/imdbdatabase";
+    public static final String DATABASE_CONNECTION = "jdbc:mysql://localhost:3306/instalitedb";
     public static final String DATABASE_USERNAME = "admin";
     public static final String DATABASE_PASSWORD = "rds-password";
 
-    public static final String SPARK_APP_NAME = "IMDBRelations";
-    public static final String SPARK_MASTER_URL = "local[*]";
-    public static final String SPARK_DRIVER_MEMORY = "10g";
-    public static final String SPARK_TESTING_MEMORY = "2147480000";
+    // public static final String SPARK_APP_NAME = "IMDBRelations";
+    // public static final String SPARK_MASTER_URL = "local[*]";
+    // public static final String SPARK_DRIVER_MEMORY = "10g";
+    // public static final String SPARK_TESTING_MEMORY = "2147480000";
 
     /**
      * The path to the space-delimited social network data
+     * //
      */
-    public static String LOCAL_SPARK = "local[*]";
+    // public static String LOCAL_SPARK = "local[*]";
 
-    // Retrieve AWS credentials from environment variables
-    public static String ACCESS_KEY_ID = System.getenv("ACCESS_KEY_ID");
-    public static String SECRET_ACCESS_KEY = System.getenv("SECRET_ACCESS_KEY");
-    public static String SESSION_TOKEN = System.getenv("SESSION_TOKEN");
+    // // Retrieve AWS credentials from environment variables
+    // public static String ACCESS_KEY_ID = System.getenv("ACCESS_KEY_ID");
+    // public static String SECRET_ACCESS_KEY = System.getenv("SECRET_ACCESS_KEY");
+    // public static String SESSION_TOKEN = System.getenv("SESSION_TOKEN");
 
     /**
      * How many RDD partitions to use?

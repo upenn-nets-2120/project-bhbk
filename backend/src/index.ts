469b6d202a92e849c5deb2c29efbb300adc3cb48
bachtran <trangiabach22@gmail.com>
2024-05-09 04:39:19 -0400

    revert

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = 443;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);

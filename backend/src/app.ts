a7d1bd896f1cd98a9a54e648c9fd1a5b869dd610
bachtran <trangiabach22@gmail.com>
2024-05-09 04:16:10 -0400

    add methods

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -46,7 +46,7 @@ app.use(
   })
 );
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true, methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"] }));
 
 app.use(express.json());
 

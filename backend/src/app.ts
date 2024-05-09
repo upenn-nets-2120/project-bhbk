fdab45815f4b192b4369fd57a572c11c91479dfd
bachtran <trangiabach22@gmail.com>
2024-05-09 03:49:44 -0400

    add new stuff

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -40,7 +40,7 @@ app.use(
     secret: "supersecret",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24, secure: false },
+    cookie: { maxAge: 1000 * 60 * 60 * 24, secure: false, sameSite: 'none' },
     store: new MemoryStore(),
     proxy: true
   })

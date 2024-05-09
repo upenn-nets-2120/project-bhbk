22ea23e69895515f0bb50e930e96853af4e4c69e
bachtran <trangiabach22@gmail.com>
2024-05-09 04:40:29 -0400

    fix base url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://52.90.65.185:8000";
+export const BASE_URL = "http://localhost:8000";
 
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,

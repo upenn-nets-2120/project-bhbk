2d5c314d191803f1798bbe0eb4d5effb89207fba
bachtran <trangiabach22@gmail.com>
2024-05-09 04:08:17 -0400

    set origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -46,7 +46,7 @@ app.use(
   })
 );
 
-app.use(cors({ credentials: true, preflightContinue: true }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 app.use(express.json());
 
